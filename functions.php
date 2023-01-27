77282f954283b03a5bb64b2dbf442227c6f214ff
Winnie Li <winnieli535@gmail.com>
2023-01-26 23:12:11 -0500

    Ajout de add_action pour enfiler style.css dans functions.php

--- functions.php
+++ functions.php
@@ -3,8 +3,12 @@
 * L'ensemble des fonctions du thème
 */
 
+function enfiler_css() {
 wp_enqueue_style('4w4-principal', // Identificateur
-get_template_directory_uri() . '/style.css', // Adresse URL de style.css
-array(), // Définir les dépendances
-filemtime(get_template_directory() . '/style.css'), // Le calcul de la version du fichier css
-'all'); // Media
+        get_template_directory_uri() . '/style.css', // L'adresse url de style.css
+        array(), // Définir les dépendances
+        filemtime(get_template_directory() . '/style.css'), // Le calcul de la version du fichier css
+        'all'); // Media
+}
+
+add_action( 'wp_enqueue_scripts', 'enfiler_css' );
