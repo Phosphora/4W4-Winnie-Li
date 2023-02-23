15189e556fa3902d1d18019b2747f40d5379dd44
Winnie Li <winnieli535@gmail.com>
2023-02-23 13:03:19 -0500

    s5c2 search.php, category.php et la classe WP_query()

---
+++ 404.php
@@ -0,0 +1,14 @@
+<?php 
+/**
+ * 404.php est le modèle par défaut pour le message d'erreur du thème 4W4.
+ */
+?>
+
+<?php get_header(); ?>
+    <main>
+        <pre>404.php</pre>
+        <h1>Erreur 404</h1>
+    </main>
+    <?php get_footer(); ?>
+</body>
+</html>
