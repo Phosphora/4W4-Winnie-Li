3d5d89e5a17b8c677065ac2370982f019c6c8983
Winnie Li <winnieli535@gmail.com>
2023-03-30 09:50:58 -0400

    s9c2 Ajustement du titre principal et Ajout du sous-titre

--- 404.php
+++ 404.php
@@ -8,6 +8,7 @@
     <main class="site__main">
         <pre>404.php</pre>
         <h1>Erreur 404</h1>
+        <h2>Page introuvable, vous pouvez tenter une recherche</h2>
     </main>
     <?php get_footer(); ?>
 </body>
