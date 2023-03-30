2c6ec5bf81a20174911f4aba42d9655f697139c8
Winnie Li <winnieli535@gmail.com>
2023-03-30 10:22:31 -0400

    s9c2 Ajout de la formulaire de recherche dans la page d'erreur

--- 404.php
+++ 404.php
@@ -9,6 +9,9 @@
         <pre>404.php</pre>
         <h1>Erreur 404</h1>
         <h2>Page introuvable, vous pouvez tenter une recherche</h2>
+        <div class="erreur__recherche">
+            <?= get_search_form() ?>
+        </div>
     </main>
     <?php get_footer(); ?>
 </body>
